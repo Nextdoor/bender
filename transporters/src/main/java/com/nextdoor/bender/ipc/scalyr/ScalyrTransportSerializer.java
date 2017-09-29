5e993f2dc5af46ae9304eec1f22c1e609b8f22bb
Matt Wise <matt@nextdoor.com>
2017-09-29 09:54:54 -0700

    Set $Year

--- transporters/src/main/java/com/nextdoor/bender/ipc/scalyr/ScalyrTransportSerializer.java
+++ transporters/src/main/java/com/nextdoor/bender/ipc/scalyr/ScalyrTransportSerializer.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright $year Nextdoor.com, Inc
+ * Copyright 2017 Nextdoor.com, Inc
  */
 
 package com.nextdoor.bender.ipc.scalyr;
