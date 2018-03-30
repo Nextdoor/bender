4a9ff6d56f075ecebc768439357647113bf6df62
slava <slava@nextdoor.com>
2018-03-30 01:06:57 +0000

    Add a generic substitution operation and gelf operation

---
+++ operations/src/main/java/com/nextdoor/bender/operation/gelf/GelfOperationFactory.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright 2017 Nextdoor.com, Inc
+ *
+ */
+
+package com.nextdoor.bender.operation.gelf;
+
+import java.util.ArrayList;
+
+import com.nextdoor.bender.config.AbstractConfig;
+import com.nextdoor.bender.operation.OperationFactory;
+import com.nextdoor.bender.operation.substitution.SubstitutionOperation;
+import com.nextdoor.bender.operation.substitution.SubstitutionSpec;
+import com.nextdoor.bender.operation.substitution.SubstitutionSpec.Interpreter;
+
+/**
+ * Create a {@link SubstitutionOperation} with GELF defaults.
+ */
+public class GelfOperationFactory implements OperationFactory {
+  private GelfOperationConfig config;
+  private ArrayList<SubstitutionSpec> subSpecs;
+
+  @Override
+  public SubstitutionOperation newInstance() {
+    return new SubstitutionOperation(this.subSpecs);
+  }
+
+  @Override
+  public Class<SubstitutionOperation> getChildClass() {
+    return SubstitutionOperation.class;
+  }
+
+  @Override
+  public void setConf(AbstractConfig config) {
+    this.config = (GelfOperationConfig) config;
+
+    ArrayList<SubstitutionSpec> subSpecs = new ArrayList<SubstitutionSpec>();
+    this.subSpecs = subSpecs;
+
+    subSpecs.add(new SubstitutionSpec("version", "1.1", Interpreter.STATIC));
+
+    if (this.config.getSrcHostField() != null) {
+      subSpecs.add(new SubstitutionSpec("host", this.config.getSrcHostField(), Interpreter.FIELD));
+    }
+
+    if (this.config.getSrcShortMessageField() != null) {
+      subSpecs.add(new SubstitutionSpec("short_message", this.config.getSrcShortMessageField(),
+          Interpreter.FIELD));
+    }
+
+    if (this.config.getSrcFullMessageField() != null) {
+      subSpecs.add(new SubstitutionSpec("full_message", this.config.getSrcFullMessageField(),
+          Interpreter.FIELD));
+    }
+
+    if (this.config.getSrcTimestampField() != null) {
+      subSpecs.add(
+          new SubstitutionSpec("timestamp", this.config.getSrcTimestampField(), Interpreter.FIELD));
+    }
+
+    if (this.config.getSrcLevelField() != null) {
+      subSpecs
+          .add(new SubstitutionSpec("level", this.config.getSrcLevelField(), Interpreter.FIELD));
+    }
+
+
+    if (this.config.getSrcFacilityField() != null) {
+      subSpecs.add(
+          new SubstitutionSpec("facility", this.config.getSrcFacilityField(), Interpreter.FIELD));
+    }
+
+    if (this.config.getSrcLineNumberField() != null) {
+      subSpecs.add(
+          new SubstitutionSpec("line", this.config.getSrcLineNumberField(), Interpreter.FIELD));
+    }
+
+    if (this.config.getSrcFileField() != null) {
+      subSpecs.add(new SubstitutionSpec("file", this.config.getSrcFileField(), Interpreter.FIELD));
+    }
+  }
+}
---
+++ operations/src/main/java/com/nextdoor/bender/operation/substitution/SubstitutionSpec.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright 2018 Nextdoor.com, Inc
+ *
+ */
+
+package com.nextdoor.bender.operation.substitution;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDefault;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
+
+public class SubstitutionSpec {
+  @JsonSchemaDescription("Name of the new field")
+  @JsonProperty(required = true)
+  private String key;
+
+  @JsonSchemaDescription("Value of the new field")
+  @JsonProperty(required = true)
+  private String value;
+
+  @JsonSchemaDescription("Interpreter of the value. If FIELD then the field value will be pulled from the event.")
+  @JsonProperty(required = true)
+  @JsonSchemaDefault("STRING")
+  private Interpreter interpreter = Interpreter.STATIC;
+
+  public enum Interpreter {
+    STATIC, FIELD
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getKey() {
+    return this.key;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return this.value;
+  }
+
+  public Interpreter getInterpreter() {
+    return this.interpreter;
+  }
+
+  public SubstitutionSpec(String key, String value, Interpreter interpreter) {
+    this.key = key;
+    this.value = value;
+    this.interpreter = interpreter;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof SubstitutionSpec)) {
+      return false;
+    }
+
+    SubstitutionSpec other = (SubstitutionSpec) o;
+
+    if (this.key != other.getKey()) {
+      return false;
+    }
+
+    if (this.value != other.getValue()) {
+      return false;
+    }
+
+    if (this.interpreter != other.getInterpreter()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.key, this.value, this.interpreter);
+  }
+
+  public String toString() {
+    return this.key + ":" + this.value + ":" + this.interpreter.toString();
+  }
+}
