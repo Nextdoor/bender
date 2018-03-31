92588a55c7ec85ca37af5f165ba0a5542e035fde
slava <slava@nextdoor.com>
2018-03-31 00:32:37 +0000

    Add optional SSL verification and fixes for json

--- transporters/src/main/java/com/nextdoor/bender/ipc/tcp/TcpTransportFactory.java
+++ transporters/src/main/java/com/nextdoor/bender/ipc/tcp/TcpTransportFactory.java
@@ -23,8 +23,17 @@ import com.nextdoor.bender.ipc.TransportFactoryInitException;
 import com.nextdoor.bender.ipc.generic.GenericTransportSerializer;
 import java.io.IOException;
 import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import okio.Okio;
 import okio.Sink;
 
@@ -49,7 +58,13 @@ public class TcpTransportFactory implements TransportFactory {
     try {
       Socket socket;
       if (config.getUseSSL()) {
-        socket = SSLSocketFactory.getDefault().createSocket(config.getHostname(), config.getPort());
+        if (config.getVerifySSL()) {
+          socket =
+              SSLSocketFactory.getDefault().createSocket(config.getHostname(), config.getPort());
+        } else {
+          socket = getSSLContext().getSocketFactory().createSocket(config.getHostname(),
+              config.getPort());
+        }
       } else {
         socket = new Socket(config.getHostname(), config.getPort());
       }
@@ -62,14 +77,53 @@ public class TcpTransportFactory implements TransportFactory {
     }
   }
 
+  /**
+   * There isn't an easy way in java to trust non-self signed certs. Just allow all until java
+   * KeyStore functionality is added to Bender.
+   *
+   * @return a context that trusts all SSL certs
+   */
+  private SSLContext getSSLContext() {
+    /*
+     * Create SSLContext and TrustManager that will trust all SSL certs.
+     *
+     * Copy pasta from http://stackoverflow.com/a/4837230
+     */
+    TrustManager tm = new X509TrustManager() {
+      public void checkClientTrusted(X509Certificate[] chain, String authType)
+          throws CertificateException {}
+
+      public void checkServerTrusted(X509Certificate[] chain, String authType)
+          throws CertificateException {}
+
+      public X509Certificate[] getAcceptedIssuers() {
+        return null;
+      }
+    };
+
+    SSLContext ctx;
+    try {
+      ctx = SSLContext.getInstance("TLS");
+    } catch (NoSuchAlgorithmException e) {
+      throw new TransportFactoryInitException("JVM does not have proper libraries for TSL");
+    }
+
+    try {
+      ctx.init(null, new TrustManager[] {tm}, new java.security.SecureRandom());
+    } catch (KeyManagementException e) {
+      throw new TransportFactoryInitException("Unable to init SSLContext with TrustManager", e);
+    }
+    return ctx;
+  }
+
+
   @Override
   public TransportBuffer newTransportBuffer() {
     return new TcpTransportBuffer(config.getMaxBufferSize(), new GenericTransportSerializer());
   }
 
   @Override
-  public void close() {
-  }
+  public void close() {}
 
   @Override
   public int getMaxThreads() {
