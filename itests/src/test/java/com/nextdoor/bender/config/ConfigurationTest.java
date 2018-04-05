eeb269667c071f7552d7a89736703463d85236ea
slava <slava@nextdoor.com>
2018-04-05 15:38:52 +0000

    Fix license comments

--- itests/src/test/java/com/nextdoor/bender/config/ConfigurationTest.java
+++ itests/src/test/java/com/nextdoor/bender/config/ConfigurationTest.java
@@ -9,7 +9,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  *
- * Copyright $year Nextdoor.com, Inc
+ * Copyright 2018 Nextdoor.com, Inc
  *
  */
 
