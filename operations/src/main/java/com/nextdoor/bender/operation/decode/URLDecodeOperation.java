6e80e2ca079bf7a519adb38eb8a77ee099fcbf81
slava <slava@nextdoor.com>
2018-04-25 17:48:06 +0000

    URL Decode operation

---
+++ operations/src/main/java/com/nextdoor/bender/operation/decode/URLDecodeOperation.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright 2018 Nextdoor.com, Inc
+ *
+ */
+
+package com.nextdoor.bender.operation.decode;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.net.URLCodec;
+
+import com.nextdoor.bender.InternalEvent;
+import com.nextdoor.bender.operation.Operation;
+
+/**
+ * URL Decodes specified fields.
+ */
+public class URLDecodeOperation implements Operation {
+  public static final URLCodec codec = new URLCodec();
+  private final List<String> fieldNames;
+  private final int times;
+
+  public URLDecodeOperation(List<String> fieldNames, int times) {
+    this.fieldNames = fieldNames;
+    this.times = times;
+  }
+
+  @Override
+  public InternalEvent perform(InternalEvent ievent) {
+    if (ievent == null || ievent.getEventObj() == null
+        || ievent.getEventObj().getPayload() == null) {
+      return ievent;
+    }
+
+    for (String fieldName : fieldNames) {
+      /*
+       * Get field value
+       */
+      String value = null;
+      try {
+        value = ievent.getEventObj().getFieldAsString(fieldName);
+      } catch (NoSuchElementException e) {
+        continue;
+      }
+
+      if (value == null) {
+        return ievent;
+      }
+
+      /*
+       * Perform decode multiple times if required.
+       */
+      try {
+        for (int i = 0; i < this.times; i++) {
+          value = codec.decode(value);
+        }
+      } catch (DecoderException e) {
+        continue;
+      }
+
+      /*
+       * Update value
+       */
+      ievent.getEventObj().setField(fieldName, value);
+    }
+
+    return ievent;
+  }
+}
